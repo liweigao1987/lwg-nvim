868cd136df3475d7730b8e23914de60c6964d5b4
liweigao <liweigao@meituan.com>
2025-09-02 18:10:51 +0800

    dashboard图案更新

--- nvim/lua/lazy-manager.lua
+++ nvim/lua/lazy-manager.lua
@@ -143,7 +143,33 @@ require("lazy").setup({
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 			bigfile = { enabled = true },
-			dashboard = { enabled = true },
+			dashboard = {
+pane_gap = 20,
+preset = {
+       header = [[
+ __                               
+/  |                              
+$$ |       __   __   __   ______  
+$$ |      /  | /  | /  | /      \ 
+$$ |      $$ | $$ | $$ |/$$$$$$  |
+$$ |      $$ | $$ | $$ |$$ |  $$ |
+$$ |_____ $$ \_$$ \_$$ |$$ \__$$ |
+$$       |$$   $$   $$/ $$    $$ |
+$$$$$$$$/  $$$$$/$$$$/   $$$$$$$ |
+                        /  \__$$ |
+                        $$    $$/ 
+                         $$$$$$/
+]]
+},
+sections = {
+    { section = "header" },
+    { section = "keys", gap = 1 },
+    { section = "startup", padding = {0, 2} },
+    { pane = 2, section = "header" },
+    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
+    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+  },
+},
 			indent = { enabled = true },
 			input = { enabled = true },
 			notifier = { enabled = true },
