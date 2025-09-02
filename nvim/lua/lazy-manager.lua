af62973e5f4268cbffb33e9275a345e046335e1b
liweigao <liweigao@meituan.com>
2025-09-02 18:59:20 +0800

    格式化

--- nvim/lua/lazy-manager.lua
+++ nvim/lua/lazy-manager.lua
@@ -144,32 +144,32 @@ require("lazy").setup({
 			-- refer to the configuration section below
 			bigfile = { enabled = true },
 			dashboard = {
-pane_gap = 20,
-preset = {
-       header = [[
- __                               
-/  |                              
-$$ |       __   __   __   ______  
-$$ |      /  | /  | /  | /      \ 
+				pane_gap = 20,
+				preset = {
+					header = [[
+ __
+/  |
+$$ |       __   __   __   ______
+$$ |      /  | /  | /  | /      \
 $$ |      $$ | $$ | $$ |/$$$$$$  |
 $$ |      $$ | $$ | $$ |$$ |  $$ |
 $$ |_____ $$ \_$$ \_$$ |$$ \__$$ |
 $$       |$$   $$   $$/ $$    $$ |
 $$$$$$$$/  $$$$$/$$$$/   $$$$$$$ |
                         /  \__$$ |
-                        $$    $$/ 
+                        $$    $$/
                          $$$$$$/
 ]]
-},
-sections = {
-    { section = "header" },
-    { section = "keys", gap = 1 },
-    { section = "startup", padding = {0, 2} },
-    { pane = 2, section = "header" },
-    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
-    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
-  },
-},
+				},
+				sections = {
+					{ section = "header" },
+					{ section = "keys", gap = 1 },
+					{ section = "startup", padding = { 0, 2 } },
+					{ pane = 2, section = "header" },
+					{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
+					{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+				},
+			},
 			indent = { enabled = true },
 			input = { enabled = true },
 			notifier = { enabled = true },
