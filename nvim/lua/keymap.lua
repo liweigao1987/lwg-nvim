f6edcf89d02b4e21c16119ee919227cdc27169cf
liweigao <liweigao@meituan.com>
2025-09-04 15:02:22 +0800

    所有自定义快捷键增加leader键

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -23,11 +23,11 @@ map("i", "<C-j>", "<down>", opt)
 map("i", "<C-k>", "<up>", opt)
 map("i", "<C-l>", "<right>", opt)
 
-map("n", "w/", ":vsp<cr>", opt)
-map("n", "w-", ":sp<cr>", opt)
-map("n", "wd", "<C-w>c", opt)
-map("n", "wD", "<C-w>o", opt)
-map("n", "wo", "<C-w>w", opt)
+map("n", "<leader>w/", ":vsp<cr>", opt)
+map("n", "<leader>w-", ":sp<cr>", opt)
+map("n", "<leader>wd", "<C-w>c", opt)
+map("n", "<leader>wD", "<C-w>o", opt)
+map("n", "<leader>wo", "<C-w>w", opt)
 --------------------------------------------------------------------
 -- 插件快捷键
 local pluginKeys = {}
@@ -53,34 +53,34 @@ pluginKeys.telescopeList = {
 	},
 }
 local builtin = require('telescope.builtin')
-map('n', 'ff', builtin.find_files, {})
-map('n', 'fg', builtin.live_grep, {})
-map('n', 'fb', builtin.buffers, {})
-map('n', 'fh', builtin.help_tags, {})
-map('n', 'fu', builtin.current_buffer_fuzzy_find, {})
-map('n', 'fw', function()
+map('n', '<leader>ff', builtin.find_files, {})
+map('n', '<leader>fg', builtin.live_grep, {})
+map('n', '<leader>fb', builtin.buffers, {})
+map('n', '<leader>fh', builtin.help_tags, {})
+map('n', '<leader>fu', builtin.current_buffer_fuzzy_find, {})
+map('n', '<leader>fw', function()
 	builtin.grep_string({ search = vim.fn.expand("<cword>"), path = "%" })
 end, {})
-map('n', '<leader>p', ":Telescope project<CR>", opt)
+map('n', '<leader>pp', ":Telescope project<CR>", opt)
 
 
 -- nvim-tree
-map("n", "ft", ":NvimTreeToggle<CR>", opt)
+map("n", "<leader>ft", ":NvimTreeToggle<CR>", opt)
 
 -- bufferline
-map("n", "bb", ":BufferLinePick<CR>", opt)
+map("n", "<leader>bb", ":BufferLinePick<CR>", opt)
 map("n", "<leader><TAB>", ":BufferLineGoToBuffer -1<cr>", opt)
 -- 左右Tab切换
-map("n", "bh", ":BufferLineCyclePrev<CR>", opt)
-map("n", "bl", ":BufferLineCycleNext<CR>", opt)
+map("n", "<leader>bh", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<leader>bl", ":BufferLineCycleNext<CR>", opt)
 -- "moll/vim-bbye" 关闭当前 buffer
-map("n", "bd", ":bdelete!<CR>", opt)
+map("n", "<leader>bd", ":bdelete!<CR>", opt)
 -- 关闭其他标签页
-map("n", "bo", ":BufferLineCloseOthers<CR>", opt)
+map("n", "<leader>bo", ":BufferLineCloseOthers<CR>", opt)
 
 -- nvim-window
 local nvimWindow = require('nvim-window')
-map('n', 'ww', nvimWindow.pick, { desc = "window pick" })
+map('n', '<leader>ww', nvimWindow.pick, { desc = "window pick" })
 -- map("n", "wo", ":lua require('nvim-window').pick()<CR>", opt)
 
 -- flash
@@ -91,17 +91,20 @@ map('o', 'r', function() flash.remote() end, { desc = "remote flash" })
 map({ 'o', 'x' }, 'R', function() flash.treesitter_search() end, { desc = "treesitter search" })
 map('c', '<C-s>', function() flash.toggle() end, { desc = "toggle flash search" })
 
+-- multicursor
+map('n', '<leader>se', ":MCstart<CR>", opt)
+
 -- comment
 pluginKeys.comment = {
 	-- Normal 模式快捷键
 	toggler = {
-		line = "cl", -- 行注释
-		block = "cb", -- 块注释
+		line = "<leader>cl", -- 行注释
+		block = "<leader>cb", -- 块注释
 	},
 	-- Visual 模式
 	opleader = {
-		line = "cl",
-		block = "cb",
+		line = "<leader>cl",
+		block = "<leader>cb",
 	},
 }
 
@@ -124,17 +127,17 @@ pluginKeys.mapLSP = function(mapbuf)
     mapbuf('n', 'gd', '<cmd>Lspsaga preview_definition<CR>', opt)
   mapbuf("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
   --]]
-	mapbuf("n", "gd", "<cmd>lua require'telescope.builtin'.lsp_definitions({ initial_mode = 'normal', })<CR>", opt)
+	mapbuf("n", "<leader>gd", "<cmd>lua require'telescope.builtin'.lsp_definitions({ initial_mode = 'normal', })<CR>", opt)
 	--[[
   mapbuf("n", "gh", "<cmd>Lspsaga hover_doc<cr>", opt)
   Lspsaga 替换 gh
   --]]
-	mapbuf("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opt)
+	mapbuf("n", "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opt)
 	--[[
   Lspsaga 替换 gr
   mapbuf("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
   --]]
-	mapbuf("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", opt)
+	mapbuf("n", "<leader>gr", "<cmd>Lspsaga lsp_finder<CR>", opt)
 	--[[
   Lspsaga 替换 gp, gj, gk
   mapbuf("n", "gp", "<cmd>lua vim.diagnostic.open_float()<CR>", opt)
@@ -142,11 +145,11 @@ pluginKeys.mapLSP = function(mapbuf)
   mapbuf("n", "gk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opt)
   --]]
 	-- diagnostic
-	mapbuf("n", "gp", "<cmd>Lspsaga show_line_diagnostics<CR>", opt)
-	mapbuf("n", "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", opt)
-	mapbuf("n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opt)
+	mapbuf("n", "<leader>gp", "<cmd>Lspsaga show_line_diagnostics<CR>", opt)
+	mapbuf("n", "<leader>gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", opt)
+	mapbuf("n", "<leader>gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opt)
 	-- mapbuf("n", "<leader>f", ":echo \"lwg\"<cr>", opt)
-	mapbuf("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<CR>", opt)
+	mapbuf("n", "<leader>fm", "<cmd>lua vim.lsp.buf.format()<CR>", opt)
 	-- 未用
 	-- mapbuf("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opt)
 	-- mapbuf("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
