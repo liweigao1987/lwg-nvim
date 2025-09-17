b70e1a7c4ca75eeee4b5410a08c13789890555ce
liweigao <liweigao@meituan.com>
2025-09-17 15:21:24 +0800

    snacks scope

--- nvim/lua/plugin/snacks.lua
+++ nvim/lua/plugin/snacks.lua
@@ -40,6 +40,7 @@ $$$$$$$$/  $$$$$/$$$$/   $$$$$$$ |
 	input = { enabled = true },
 	notifier = { enabled = true },
 	quickfile = { enabled = true },
+	scope = { enabled = true },
 	scroll = { enabled = true },
 	statuscolumn = { enabled = true },
 	words = { enabled = true },
